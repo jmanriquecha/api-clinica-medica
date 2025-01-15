dd1f3dd50a91eba1cec1cdd940670b26abdc430e
Jorge Manrique <georgemanc.28@gmail.com>
2025-01-15 12:11:16 -0500

    Se agregar respuesta HTTP 204 al cliente al eliminar médico

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -36,9 +37,10 @@ public class MedicoController {
     @DeleteMapping("/{id}")
     @Transactional
     // Elimina datos de manera lógica
-    public void eliminarMedico(@PathVariable Long id){
+    public ResponseEntity eliminarMedico(@PathVariable Long id){
         Medico medico = medicoRepository.getReferenceById(id);
         medico.desactivarMedico();
+        return ResponseEntity.noContent().build(); // Respuesta HTTP 204
     }
     // Elimina los datos de la base de datos
 //    public void eliminarMedico(@PathVariable Long id){
