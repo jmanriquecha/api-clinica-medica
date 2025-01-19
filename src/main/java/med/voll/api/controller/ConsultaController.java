9fd82a7b5b4d10226a596ee26da2cc4539d67013
Jorge Manrique <georgemanc.28@gmail.com>
2025-01-19 15:46:30 -0500

    Se inicia la construcción de nuevo Requisito para agregar consultas

---
+++ src/main/java/med/voll/api/controller/ConsultaController.java
@@ -0,0 +1,28 @@
+package med.voll.api.controller;
+
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import med.voll.api.domain.consulta.ConsultaRepository;
+import med.voll.api.domain.consulta.DatosDetalleConsulta;
+import med.voll.api.domain.consulta.DatosReservaConsulta;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/consultas")
+public class ConsultaController {
+
+    @Autowired
+    private ConsultaRepository consultaRepository;
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity reservar(@RequestBody @Valid DatosReservaConsulta datos){
+        System.out.println(datos);
+        return ResponseEntity.ok(new DatosDetalleConsulta(null, null, null, null));
+    }
+}
