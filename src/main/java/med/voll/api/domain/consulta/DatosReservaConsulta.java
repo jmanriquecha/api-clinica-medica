eecde28fd66e396d512b231f2095f7e34eabd9ec
Jorge Manrique <georgemanc.28@gmail.com>
2025-01-19 22:47:02 -0500

    Se crea consulta JPQL para traer médicos aleatorios y con disponibilidad en una fecha

--- src/main/java/med/voll/api/domain/consulta/DatosReservaConsulta.java
+++ src/main/java/med/voll/api/domain/consulta/DatosReservaConsulta.java
@@ -3,6 +3,7 @@ package med.voll.api.domain.consulta;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotNull;
+import med.voll.api.domain.medico.Especialidad;
 
 import java.time.LocalDateTime;
 
@@ -13,6 +14,7 @@ public record DatosReservaConsulta(
         @NotNull
         @Future
 //        @JsonFormat(pattern = "dd/MM/yyyy HH:mm") // Personaliza formato de fecha
-        LocalDateTime fecha
+        LocalDateTime fecha,
+        Especialidad especialidad
 ) {
 }
